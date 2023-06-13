d8f97f261cc82bdf8a4e5c959dd972ebeba279bc
inspiringsource <ab.b2k3@gmail.com>
2023-06-13 21:16:36 +0200

    build(deps): update api/index.py and package-lock.json

--- api/index.py
+++ api/index.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
+class University(BaseModel):
+    name: str
+    country: str
+
 @app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+def read_root():
+    response = requests.get('http://universities.hipolabs.com/search?country=switzerland')
+    data = response.json()
+    universities = [University(**item) for item in data]
+    university_names = [university.name for university in universities]
+    return {"university_names": university_names}
+
